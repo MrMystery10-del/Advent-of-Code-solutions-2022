bb99e8852a5242a7cf42344d6d5c1486d789fe0a
Mr Mystery 1.0 <69641472+MrMystery10-del@users.noreply.github.com>
2022-12-01 15:31:27 +0100

    Uploaded solution for Day 1

---
+++ AoC_Day1_Solution/Solution.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+
+  private BufferedReader reader;
+  private FileReader input;
+
+  private ArrayList<Integer> elfs = new ArrayList<>();
+  private Boolean finsihed = false;
+  private int index = -1;
+
+  public Main() {
+    setReader();
+    while (!finsihed) {
+      count();
+    }
+    elfs.sort(null);
+    System.out.println("Result for top 1: " + elfs.get(index));
+    System.out.println("Result for top 3 total: " + (elfs.get(index) + elfs.get(index - 1) + elfs.get(index - 2)));
+  }
+
+  // Count all the calories from each elf
+  private void count() {
+    String calories = "";
+    int temp = 0;
+    try {
+      while (!(calories = reader.readLine()).isEmpty()) {
+        temp += Integer.parseInt(calories);
+      }
+      elfs.add(temp);
+      index++;
+    } catch (IOException exception) {
+      exception.printStackTrace();
+    } catch (NullPointerException nullPointerException) {
+      finsihed = true;
+    }
+  }
+
+  // This method gets the input from a txt file
+  private void setReader() {
+    try {
+      input = new FileReader(new File("INPUT.txt"));
+    } catch (FileNotFoundException exception) {
+      exception.printStackTrace();
+    }
+    reader = new BufferedReader(input);
+  }
+
+  public static void main(String[] args) {
+    new Main();
+  }
+}
